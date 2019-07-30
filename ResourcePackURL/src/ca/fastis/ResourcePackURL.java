9b0f135737e3a5d845cb6c8a11b38b7abf89dc8c
BuildTools <unconfigured@null.spigotmc.org>
2019-07-30 02:32:28 -0400

    First and probably last release, fully working

--- ResourcePackURL/src/ca/fastis/ResourcePackURL.java
+++ ResourcePackURL/src/ca/fastis/ResourcePackURL.java
@@ -1,30 +1,35 @@
 package ca.fastis;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
+import java.net.UnknownHostException;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ResourcePackURL extends JavaPlugin implements Listener  {
 	static Server server;
 	static ConsoleCommandSender console;
+	static Plugin plugin;
 
 	public void onEnable() {
+		plugin = this;
 		server = this.getServer();
 		console = server.getConsoleSender();
 		server.getPluginManager().registerEvents(this, this);
 		console.sendMessage(ChatColor.GREEN + "" + ChatColor.ITALIC + "Resource Pack URL Loaded");
 	}
-	
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		getResourcePack(event.getPlayer());
@@ -32,24 +37,27 @@ public class ResourcePackURL extends JavaPlugin implements Listener  {
 
 
 	public static void getResourcePack(Player player) {
-		URL url = null;
 		try {
-			url = new URL("http://fastis.ca:8080/SimplyPerfectVersion.txt");
-		} catch (MalformedURLException e1) {
-			e1.printStackTrace();
-		}
-		String PackVersion = "";
-		
-		try {
-			if(url != null) {
-				@SuppressWarnings("resource")
-				Scanner s = new Scanner(url.openStream());
-				PackVersion = s.nextLine();
+			File path = new File("plugins//ResourcePackURL");
+			File fileYourFile = new File("plugins//ResourcePackURL//Config.yml");
+			YamlConfiguration ymlYourFile = YamlConfiguration.loadConfiguration(fileYourFile);
+			if(!path.exists()) path.mkdir();
+			if(!fileYourFile.exists()) plugin.saveResource("Config.yml", true);
+			String resourcePackURL = ymlYourFile.getString("ResourcePackURL");
+			if(resourcePackURL == null ||resourcePackURL.isEmpty()) {
+				return;
+			} else {
+				final URL url = new URL(ymlYourFile.getString("ResourcePackURL"));
+				HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+				int responseCode = huc.getResponseCode();
+				if(responseCode == 200) player.setResourcePack(url.toString());
 			}
-			player.setResourcePack("http://fastis.ca:8080/SimplyPerfectv" + PackVersion + ".zip");
-			
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (UnknownHostException uhe) {
+			console.sendMessage(ChatColor.RED + "Resource Pack URL UnknownHostException : " + uhe.getMessage());
+		} catch (FileNotFoundException fnfe) {
+			console.sendMessage(ChatColor.RED + "Resource Pack URL FileNotFoundException : " + fnfe.getMessage());
+		} catch (Exception e) {
+			console.sendMessage(ChatColor.RED + "Resource Pack URL Exception : " + e.getMessage());
 		}
 	}
 
